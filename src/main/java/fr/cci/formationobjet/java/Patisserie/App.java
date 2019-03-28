9c41e41374a2f520eead49dba5eac32e36bff008
haider <shatihaider@gmail.com>
2019-03-28 10:43:17 +0100

    initial commit

---
+++ src/main/java/fr/cci/formationobjet/java/Patisserie/App.java
@@ -0,0 +1,13 @@
+package fr.cci.formationobjet.java.Patisserie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
